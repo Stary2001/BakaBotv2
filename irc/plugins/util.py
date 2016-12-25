a1ea1ac020b1175a9d897af5c6736c5e5c0c65db
Ezekiel Bethel <mctinfoilball@gmail.com>
2016-12-25 18:20:45 +0000

    reminders

--- irc/plugins/util.py
+++ irc/plugins/util.py
@@ -1,6 +1,7 @@
 from command import command, CommandFlags
 from irc.plugin import Plugin
 import random
+from eventlet import greenthread
 
 class UtilPlugin(Plugin):
 	commands = {}
@@ -17,3 +18,10 @@ class UtilPlugin(Plugin):
 		choices = choices.split(' or ')
 		idx = random.randint(0, len(choices)-1)
 		return [choices[idx]]
+
+	@command(commands, 'remind', flags=[])
+	def command_remind(ctx, t, *msg):
+		msg = ' '.join(msg)
+		t = int(t)
+		greenthread.spawn_after(t, lambda: ctx.reply("timer '{}' is up!".format(msg)))
+		ctx.reply("Timer in {} for '{}' added.".format(time, msg))
