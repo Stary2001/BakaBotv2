2c8754a334e226b7b5d98681266204f8a5c48d7a
Ezekiel Bethel <mctinfoilball@gmail.com>
2017-04-29 23:11:11 +0100

    cleanup initial bot handling a bit

--- main.py
+++ main.py
@@ -9,18 +9,22 @@ import asyncio
 bots = {}
 config = None
 try:
-	config = Config('conf/main.yml')
+    config = Config('conf/main.yml')
+    shared_conf = Config('conf/shared.yml')
 except FileNotFoundError:
-	print("config not found :(")
-	exit()
+    print("config not found :(")
+    exit()
 
-for n in config.get('irc.networks'):
-	bots[n] = IRCBot(n, shared_config = conf)
+if config.get('bots.irc'):
+    for n in config.get('bots.irc'):
+       bots[n] = IRCBot(n, shared_config = shared_conf)
 
-bots['discord'] = DiscordBot('discord', shared_config = conf)
+if config.get('bots.discord'):
+    bots['discord'] = DiscordBot('discord', shared_config = shared_conf)
 
-loop = asyncio.get_event_loop()
-for n in bots:
-	bots[n].run(loop)
+if len(bots) != 0:
+    loop = asyncio.get_event_loop()
+    for n in bots:
+        bots[n].run(loop)
 
-loop.run_forever()
+    loop.run_forever()
